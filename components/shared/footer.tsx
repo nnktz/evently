f3cc803017e1d20992cb6e24bea1cef20a35ba4b
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2024-01-16 14:37:09 +0700

    Footer component

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,3 +1,16 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export const Footer = () => {
-    return <div className=""></div>
+  return (
+    <footer className="border-t">
+      <div className="wrapper flex-between flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href={'/'}>
+          <Image src={'/assets/images/logo.svg'} alt="Logo" width={128} height={38} />
+        </Link>
+
+        <p>{new Date().getFullYear()} Evently. All Rights reserved.</p>
+      </div>
+    </footer>
+  )
 }
